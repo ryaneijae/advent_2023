3155f79acd0b8542445cfee44e4b1cb8583cd372
Ryan Cho <ryan@pattern.bio>
2023-12-05 12:58:32 -0600

    Completed Day3

--- day03_GearRatios/part2.py
+++ day03_GearRatios/part2.py
@@ -40,24 +40,6 @@ class GearRatios:
                     return True
         return False
 
-    def is_gear(self, row_index, ele_index):
-        ele_range_min = ele_index - 1
-        ele_range_max = ele_index + 1
-        row_range_min = row_index - 1
-        row_range_max = row_index + 1
-        
-        if ele_range_min < 0: ele_range_min = 0
-        if row_range_min < 0: row_range_min = 0
-        if ele_range_max > self.ele_len: ele_range_max = self.ele_len - 1
-        if row_range_max >= self.row_len: row_range_max = self.row_len - 1
-        
-        count = 0
-        for row in range(row_range_min, row_range_max + 1):
-            for ele in range(ele_range_min, ele_range_max + 1):
-                if self.schematic[row, ele] in "0123456789":
-                    count = count + 1
-        print(count)
-
     def get_number_loc(self):
         number_loc = []
         for row_index in range(self.row_len):
@@ -97,13 +79,50 @@ class GearRatios:
                 total = total + int_pn
         return total
 
+    def is_gear(self, star_row, star_ele):
+        count = 0
+        gear_ratio = 1
+        for each_ele_min, each_ele_max, each_row in self.number_loc:
+            matrix = []
+            ele_range_min = each_ele_min - 1
+            ele_range_max = each_ele_max + 1
+            row_range_min = each_row - 1
+            row_range_max = each_row + 1
+        
+            if ele_range_min < 0: ele_range_min = 0
+            if row_range_min < 0: row_range_min = 0
+            if ele_range_max > self.ele_len: ele_range_max = self.ele_len - 1
+            if row_range_max >= self.row_len: row_range_max = self.row_len - 1
+
+            for row in range(row_range_min, row_range_max + 1):
+                for ele in range(ele_range_min, ele_range_max + 1):
+                    matrix.append((row, ele))
+
+            if (star_row, star_ele) in matrix:
+                count = count + 1
+                gear_ratio = gear_ratio * arr_to_int(
+                        self.schematic[each_row, each_ele_min: each_ele_max+1]
+                        )
+
+        if count == 2:
+            return gear_ratio
+        return 0        
+
+    def get_gear_ratios(self):
+        self.star_loc = self.get_star_loc()
+        self.number_loc = self.get_number_loc()
+        total = 0
+        for star_row, star_ele in self.star_loc:
+            total = total + self.is_gear(star_row, star_ele)
+        
+        return total
+
+
 def main(argv):
     gear_ratios = GearRatios(argv[0])
     gear_ratios.print()
-    ans = gear_ratios.get_star_loc()
+    ans = gear_ratios.get_gear_ratios()
     print(ans)
-    for r, c in ans:
-        gear_ratios.is_gear(r, c)
 
 
 if __name__ == '__main__':
